30ac5aec8f87361b2aead9694de27b7fcd65f319
Jelili Abdullahi <osuolaleabdullahi@gmail.com>
2024-07-11 07:23:19 +0100

    wip

--- dist/utils.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.hasCommonElement = hasCommonElement;
-function hasCommonElement(arr1, arr2) {
-    return arr1.some((element) => arr2.includes(element));
-}
