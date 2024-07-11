dc4db2f9ac3a7d9bfb1ea46e4f6fe7521e981cfc
Jelili Abdullahi <osuolaleabdullahi@gmail.com>
2024-07-11 13:15:59 +0100

    db connection log added

--- src/database/index.ts
+++ src/database/index.ts
@@ -8,4 +8,14 @@ const sequelize = new Sequelize(process.env.DATABASE_URL as string, {
   models: [__dirname + "/models"],
 });
 
+// Add connection callback
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log("Connection has been established successfully.");
+  })
+  .catch((err: Error) => {
+    console.error("Unable to connect to the database:", err);
+  });
+
 export default sequelize;
